5ee9eb8c7c1b5801ef99db602179c7e6e1fc927b
Rajat Kumar (CW) <Rajat.Kumar1@jda.com>
2024-03-12 14:23:13 +0530

    usercont

--- src/main/java/com/bank/main/controller/UserController.java
+++ src/main/java/com/bank/main/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.bank.main.controller;
 
+import com.bank.main.dto.UserResponse;
+import com.bank.main.security.JwtTokenUtil;
+import com.bank.main.service.UserDetailsService;
+import com.bank.main.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserDetailsService userDetailsService;
+    private final UserService userService;
+
 }
---
+++ src/main/java/com/bank/main/service/UserDetailsService.java
@@ -0,0 +1,5 @@
+package com.bank.main.service;
+
+public class UserDetailsService {
+
+}
